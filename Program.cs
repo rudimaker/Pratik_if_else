5e40fc4792b8260a84851248c8b79f10d0eac67b
pc <pc@DESKTOP-M7QJQNL>
2024-07-20 09:43:06 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+
+
+Console.WriteLine("Lütfen bir rakam giriniz:");
+int sayi=Convert.ToInt32(Console.ReadLine());
+
+
+if (sayi > 10 && sayi%2==0)
+{
+    Console.WriteLine("Girilen sayı 10dan büyük ve çifttir.");
+}
+else if (sayi > 10 && sayi % 2 == 1)
+{
+    Console.WriteLine("Girilen sayı 10dan büyük ve tekdir.");
+}
+else if (sayi < 10 && sayi % 2 == 0)
+{
+    Console.WriteLine("Girilen sayı 10dan büyük ve çifttir.");
+}
+else if (sayi < 10 && sayi % 2 == 1)
+{
+    Console.WriteLine("Girilen sayı 10dan büyük ve tekdir.");
+}
+else
+{
+    Console.WriteLine("Girdiğiniz sayı 10dur");
+}
